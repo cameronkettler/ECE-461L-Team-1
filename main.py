a1aa356fc718cb2fbd9e494a1042ff7109138681
andrewjli121 <andrewjli121@gmail.com>
2024-02-20 19:56:20 -0600

    mongo connected

---
+++ main.py
@@ -0,0 +1,13 @@
+#passcode: R6BykARDXo2JIEEs
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://andrewjli121:R6BykARDXo2JIEEs@ece461l.orvvqud.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
