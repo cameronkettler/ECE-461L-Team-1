683de09965b0b59dafa6830c3757b16c04bc1c8f
Nidhithippana1 <89660542+Nidhithippana1@users.noreply.github.com>
2024-03-29 14:10:46 -0500

    Update App.js
    working create project

--- frontend/hw-view/src/App.js
+++ frontend/hw-view/src/App.js
@@ -6,11 +6,25 @@ import Projects from './components/projectPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const STATE = [
-    {name: "Project 1", listAuthorizedUsers: "List of authorized users", HWSet1: "50/100", HWSet2: "20/100", alreadyJoined: false},
-    {name: "Project 2", listAuthorizedUsers: "List of authorized users", HWSet1: "50/100", HWSet2: "30/100", alreadyJoined: true},
-    {name: "Project 3", listAuthorizedUsers: "List of authorized users", HWSet1: "0/100", HWSet2: "50/100", alreadyJoined: false}
-  ];
+  const [projects, setProjects] = useState([
+    { name: "Project 1", listAuthorizedUsers: "List of authorized users", HWSet1: "50/100", HWSet2: "20/100", alreadyJoined: false },
+    { name: "Project 2", listAuthorizedUsers: "List of authorized users", HWSet1: "50/100", HWSet2: "30/100", alreadyJoined: true },
+    { name: "Project 3", listAuthorizedUsers: "List of authorized users", HWSet1: "0/100", HWSet2: "50/100", alreadyJoined: false }
+  ]);
+
+function handleCreateProject(projectData) {
+    const newProject = {
+        name: projectData.projectName,
+        listAuthorizedUsers: "Current User", //replace with current user maybe
+        HWSet1: "50/100", //replace capacity with the global one
+        HWSet2: "20/100", //replace capacity with global one
+        alreadyJoined: false
+    };
+    const updatedProjects = [...projects, newProject];
+    setProjects(updatedProjects);
+    
+  }
+
   return (
     <Router>
       <div className="App">
@@ -18,7 +32,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/project" element={<Projects currState={STATE} />} />
+            <Route path="/project" element={<Projects currState={projects} onCreateProject={handleCreateProject} />} />
           </Routes>
         </header>
       </div>
